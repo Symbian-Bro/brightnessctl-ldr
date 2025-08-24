805638033ac0c9597e270f95c5edca2000acbffe
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-24 10:19:18 +0530

    Added a mapping variable for conversion

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import serial
+import time
+import subprocess
 
 def serial_read(port='/dev/ttyUSB0', baudrate=9600, timeout=5):
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
             line = int(ser.readline().decode('utf-8').strip())
             return line
 
-ldrvalue = serial_read()
+ldrvalue = int(serial_read())
 
+percent = (ldrvalue / 1023) * 100
