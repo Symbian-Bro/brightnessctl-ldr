fc7eb519fe76f3d1de8183c159aa3565d26f3aa7
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-24 10:43:43 +0530

    Integrated the port_checker function into the main.py file.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import serial
 import time
 import subprocess
+import serial.tools.list_ports as listings
 
-def serial_read(port='/dev/ttyUSB0', baudrate=9600, timeout=5):
+def port_checker():
+    ports = listings.comports()
+    for port in ports:
+        if (('Arduino' in port.description or 'CH340' in port.description)or 
+            ('Arduino' in port.hwid or 'CH340' in port.hwid)
+        ):
+            return port.device
+    return '/dev/ttyUSB0'
+
+def serial_read(port, baudrate, timeout=5):
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
             line = int(ser.readline().decode('utf-8').strip())
             return line
