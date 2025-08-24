03c9277e633a1b7468620c13b2360193f4d08af9
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-24 10:39:38 +0530

    Added port.manufacturer and port.hwid to the port_checker function. port.description might not be enough. Corrected the indentation of the last two lines.

--- port_finder.py
+++ port_finder.py
@@ -3,10 +3,13 @@ import serial.tools.list_ports as listings
 def port_checker():
     ports = listings.comports()
     for port in ports:
-        if 'Arduino' in port.description or 'CH340' in port.description:
+        if (('Arduino' in port.description or 'CH340' in port.description)or(
+            'Arduino' in port.manufacturer or 'CH340' in port.manufacturer) or 
+            ('Arduino' in port.hwid or 'CH340' in port.hwid)
+        ):
             print(port.device)
             return port.device
-        print("The regular")
-        return '/dev/ttyUSB0'
+    print("The regular")
+    return '/dev/ttyUSB0'
 
 port_checker()
